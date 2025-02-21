cc7ef51d05c43e0b3070621e5b5e9d4631cf2278
Parth Mistri <122524279+parthmiv@users.noreply.github.com>
2025-02-21 11:20:15 +0530

    Add files via upload

---
+++ products.js
@@ -0,0 +1,86 @@
+const products = [
+    {
+        id: 101,
+        name: "Boomerang 16T | Green",
+        price: 6139,
+        image: "Images/kids1.jpeg",
+        description: "One of the most fascinating toys for the kids is a boomerang, as they enjoy throwing a boomerang and seeing it return back to start. Let your kid enjoy the same on Boomerang bike."
+    },
+    {
+        id: 102,
+        name: "Jr. King 20T | Black Orange",
+        price: 8199,
+        image: "Images/kids2.jpg",
+        description: "The Jr. King is just the bicycle you are looking for! This latest bike from Vector 91 is specifically designed for children who enjoy riding."
+    },
+    {
+        id: 103,
+        name: "Frenzy 16T | Blue Black",
+        price: 6569,
+        image: "Images/kids3.1.jpeg",
+        description: "A mix of mountain and road bike for a smooth ride."
+    },
+    {
+        id: 104,
+        name: "Figo 16T | Blue Yellow",
+        price: 8169,
+        image: "Images/kids4.1.jpeg",
+        description: "The Figo is the most trusted companion for your kid's next thrilling adventure. This stylish bike come with durable carbon steel frame, secure V-brakes, comfortable wide tires and stunning looks."
+    },
+    {
+        id: 105,
+        name: "Trooper 700C Multi Speed - Green | 18.5",
+        price: 23999,
+        image: "Images/atb1.1.jpeg",
+        description: "The exceptional All Aluminum Trooper 700C - is a performance hybrid bike for a ride to your office or your neighborhood store or just a long ride over the weekend. The straight handlebar and upright seating posture of an MTB blend with the thinner & speedier wheels of a road bike to make this stylish hybrid."
+    },
+    {
+        id: 106,
+        name: "X Life Track 700C - Black | 18inch  ",
+        price: 44999,
+        image: "Images/atb2.1.jpg",
+        description: "The KTM Life track are popular alloy hybrid bikes had allow a relaxing ride so that you can enjoy nature, equipped with suspension fork and offer more grip than the on-road brothers. Integrated cable routing and a front reflector, incorporated into the fork bridge ensure the bikes are capable and attractive. KTM always fights for the best specification to ensure a leading price-performance-ratio, worldwide. In addition to the dynamic colour design, the X like Track series also provides great riding performance thanks to high-quality components and first-class workmanship"
+    },
+    {
+        id: 107,
+        name: "Invictus 27.5T Multi Speed | Slate Grey",
+        price: 26999,
+        image: "Images/atb3.jpeg",
+        description: "Invictus is a Latin word meaning unconquered, never vanquished, or subdued. The invincible characteristics of Invictus is imbibed in this 24-speed Alloy bike and its components."
+    },
+    {
+        id: 108,
+        name: "Blizzard D2 26T | Dark",
+        price: 9169,
+        image: "Images/atb4.1.jpeg",
+        description: "The Blizzard are termed as mother of all storms. It is incredibly powerful, majestic, and extremly strong in strength. That's what you'll experience with the magnificent Blizzard."
+    },
+    {
+        id: 109,
+        name: "Wolverine X 27.5T Single Speed | Black Gold",
+        price: 25999,
+        image: "Images/ebike1.1.jpg",
+        description: "Experience the thrill of the Wolverine X electric cycle. Meticulously crafted with passion, this cycle is engineered to perfection, providing you with an exceptional ride. Embark on your journey with the ultimate ride experience."
+    },
+    {
+        id: 110,
+        name: "Meraki 29T | Matt Blue",
+        price: 28999,
+        image: "Images/ebike2.1.jpg",
+        description: "The electric cycle Meraki is created with the same passion and engineered to perfection to give you the ultimate ride experience."
+    },
+    {
+        id: 111,
+        name: "Z4 27.5T Single Speed | Black Red",
+        price: 37999,
+        image: "Images/ebike3.1.jpg",
+        description: "Experience the thrill of the Z4 electric cycle. Meticulously crafted with passion, this cycle is engineered to perfection, providing you with an exceptional ride. Embark on your journey with the ultimate ride experience."
+    },
+    {
+        id: 112,
+        name: "Z2 27.5T",
+        price: 29999,
+        image: "Images/ebike4.1.jpg",
+        description: "Discover the next level of urban mobility with the All New Z2 Electric Bicycle. Combining style and eco-friendliness, Z2 offers a smooth and efficient ride for your daily adventures. Upgrade your commute and experience a greener way to travel with Z2 today!"
+    }
+];
